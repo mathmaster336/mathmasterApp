ac28820c450477c1795c25a745f3ff4acd3d0629
Mangesh <mangeshgautam07@gmail.com>
2025-07-20 01:33:26 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function App() {
+  return (
+    <View className='w-full h-full bg-white '>
+      <Text>Hello</Text>
+    </View>
+  )
+}
+
+export default App
